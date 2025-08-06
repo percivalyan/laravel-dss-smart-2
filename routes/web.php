5212025bfd095f4d269366c020865344e04310b4
percivalyan <yoryan8888@gmail.com>
2025-08-06 18:42:26 +0700

    feat: fix route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use App\Http\Controllers\AlternativeController;
 use App\Http\Controllers\CriteriaController;
 use App\Http\Controllers\AlternativeValueController;
 use App\Http\Controllers\SubCriteriaController;
+use App\Http\Controllers\SubCriteriaNonAcademicController;
+use App\Http\Controllers\AlternativeValueNonAcademicController;
 
 Route::get('/', fn() => redirect('/login'));
 
@@ -32,12 +34,49 @@ Route::middleware([AuthOnly::class])->group(function () {
     Route::get('/users/{id}/profile', [UserController::class, 'show'])->name('users.profile');
 
     Route::resource('criteria-code', CriteriaCodeController::class);
-    Route::resource('alternative', AlternativeController::class);
+    // Academic
+    Route::get('/alternative', [AlternativeController::class, 'index'])->name('alternative.index');
+    Route::get('/alternative/create', [AlternativeController::class, 'create'])->name('alternative.create');
+    Route::post('/alternative', [AlternativeController::class, 'store'])->name('alternative.store');
+    Route::get('/alternative/edit/{id}', [AlternativeController::class, 'edit'])->name('alternative.edit');
+    Route::put('/alternative/update/{id}', [AlternativeController::class, 'update'])->name('alternative.update');
+    Route::delete('/alternative/delete/{id}', [AlternativeController::class, 'destroy'])->name('alternative.destroy');
+
+    // Non-Academic
+    Route::get('/alternative/nonacademic/create', [AlternativeController::class, 'createNonAcademic'])->name('alternative.nonacademic.create');
+    Route::post('/alternative/nonacademic', [AlternativeController::class, 'storeNonAcademic'])->name('alternative.nonacademic.store');
+    Route::get('/alternative/nonacademic/edit/{id}', [AlternativeController::class, 'editNonAcademic'])->name('alternative.nonacademic.edit');
+    Route::put('/alternative/nonacademic/update/{id}', [AlternativeController::class, 'updateNonAcademic'])->name('alternative.nonacademic.update');
+    Route::delete('/alternative/nonacademic/delete/{id}', [AlternativeController::class, 'destroyNonAcademic'])->name('alternative.nonacademic.destroy');
+
     // Route::resource('sub-criteria', SubCriteriaController::class);
     Route::get('sub-criteria', [SubCriteriaController::class, 'index'])->name('sub-criteria.index');
     Route::post('sub-criteria/bulk-update', [SubCriteriaController::class, 'bulkUpdate'])->name('sub-criteria.bulk-update');
     Route::post('sub-criteria/quick-store', [SubCriteriaController::class, 'quickStore'])->name('sub-criteria.quick-store');
-    Route::resource('criteria', CriteriaController::class);
+
+    Route::get('sub-criteriana', [SubCriteriaNonAcademicController::class, 'index'])->name('sub-criteriana.index');
+    Route::post('sub-criteriana/bulk-update', [SubCriteriaNonAcademicController::class, 'bulkUpdate'])->name('sub-criteriana.bulk-update');
+    Route::post('sub-criteriana/quick-store', [SubCriteriaNonAcademicController::class, 'quickStore'])->name('sub-criteriana.quick-store');
+
+    Route::prefix('criteria')->name('criteria.')->group(function () {
+        // Index Page
+        Route::get('/', [CriteriaController::class, 'index'])->name('index');
+
+        // Academic Criteria
+        Route::get('/create', [CriteriaController::class, 'create'])->name('create');
+        Route::post('/store', [CriteriaController::class, 'store'])->name('store');
+        Route::get('/edit/{id}', [CriteriaController::class, 'edit'])->name('edit');
+        Route::put('/update/{id}', [CriteriaController::class, 'update'])->name('update');
+        Route::delete('/destroy/{id}', [CriteriaController::class, 'destroy'])->name('destroy');
+
+        // Non-Academic Criteria
+        Route::get('/non-academic/create', [CriteriaController::class, 'createNonAcademic'])->name('nonacademic.create');
+        Route::post('/non-academic/store', [CriteriaController::class, 'storeNonAcademic'])->name('nonacademic.store');
+        Route::get('/non-academic/edit/{id}', [CriteriaController::class, 'editNonAcademic'])->name('nonacademic.edit');
+        Route::put('/non-academic/update/{id}', [CriteriaController::class, 'updateNonAcademic'])->name('nonacademic.update');
+        Route::delete('/non-academic/destroy/{id}', [CriteriaController::class, 'destroyNonAcademic'])->name('nonacademic.destroy');
+    });
+
     // Route::resource('alternative-value', AlternativeValueController::class);
     Route::get('/alternative-value', [AlternativeValueController::class, 'index'])
         ->name('alternative-value.index');
@@ -47,6 +86,15 @@ Route::middleware([AuthOnly::class])->group(function () {
 
     Route::get('/alternative-value/smart-calculate', [AlternativeValueController::class, 'smartCalculate'])
         ->name('alternative-value.smart-calculate');
+
+    Route::get('/alternative-valuena', [AlternativeValueNonAcademicController::class, 'index'])
+        ->name('alternative-valuena.index');
+
+    Route::post('/alternative-valuena/bulk-update', [AlternativeValueNonAcademicController::class, 'bulkUpdate'])
+        ->name('alternative-valuena.bulk-update');
+
+    Route::get('/alternative-valuena/smart-calculate', [AlternativeValueNonAcademicController::class, 'smartCalculate'])
+        ->name('alternative-valuena.smart-calculate');
 });
 
 Route::fallback([ErrorController::class, 'error404']);
