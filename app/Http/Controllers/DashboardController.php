bf35dd3da7abc323c6a3248b63eab10313408689
percivalyan <yoryan8888@gmail.com>
2025-08-12 23:29:43 +0700

    feat: make dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,44 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Alternative;
+use App\Models\AlternativeNonAcademic;
+use App\Models\AlternativeValue;
+use App\Models\AlternativeValueNonAcademic;
+use App\Models\Criteria;
+use App\Models\CriteriaNonAcademic;
+use App\Models\SubCriteria;
+use App\Models\SubCriteriaNonAcademic;
+use App\Models\CriteriaCode;
+
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard');
+        $totalAlternatives = Alternative::count();
+        $totalAlternativesNonAcademic = AlternativeNonAcademic::count();
+
+        $totalAlternativeValues = AlternativeValue::count();
+        $totalAlternativeValuesNonAcademic = AlternativeValueNonAcademic::count();
+
+        $totalCriterias = Criteria::count();
+        $totalCriteriasNonAcademic = CriteriaNonAcademic::count();
+
+        $totalSubCriterias = SubCriteria::count();
+        $totalSubCriteriasNonAcademic = SubCriteriaNonAcademic::count();
+
+        $totalCriteriaCodes = CriteriaCode::count();
+
+        return view('dashboard', compact(
+            'totalAlternatives',
+            'totalAlternativesNonAcademic',
+            'totalAlternativeValues',
+            'totalAlternativeValuesNonAcademic',
+            'totalCriterias',
+            'totalCriteriasNonAcademic',
+            'totalSubCriterias',
+            'totalSubCriteriasNonAcademic',
+            'totalCriteriaCodes'
+        ));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,94 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                Testing
+    <div class="content">
+        <div class="container-fluid">
+            {{-- Informasi Tentang SPK dan SMART --}}
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">Apa itu SPK & Metode SMART?</h4>
+                        </div>
+                        <div class="content">
+                            <p><strong>Sistem Pendukung Keputusan (SPK)</strong> adalah sistem berbasis komputer yang membantu
+                                dalam proses pengambilan keputusan dengan cara menganalisis data dan model yang relevan.</p>
+                            <p><strong>SMART (Simple Multi-Attribute Rating Technique)</strong> adalah salah satu metode SPK
+                                yang digunakan untuk memilih alternatif terbaik berdasarkan beberapa kriteria yang memiliki
+                                bobot dan nilai tertentu. Prosesnya melibatkan penilaian setiap alternatif terhadap setiap
+                                kriteria, lalu dikalikan dengan bobot untuk menghasilkan skor total.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Tabel Statistik --}}
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="header">
+                            <h4 class="title">Statistik Data SPK</h4>
+                        </div>
+                        <div class="content table-responsive table-full-width">
+                            <table class="table table-bordered table-striped">
+                                <thead class="text-center">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Jenis Data</th>
+                                        <th>Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Alternatif Akademik</td>
+                                        <td>{{ $totalAlternatives }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Alternatif Non-Akademik</td>
+                                        <td>{{ $totalAlternativesNonAcademic }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Nilai Alternatif Akademik</td>
+                                        <td>{{ $totalAlternativeValues }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Nilai Alternatif Non-Akademik</td>
+                                        <td>{{ $totalAlternativeValuesNonAcademic }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>Kriteria Akademik</td>
+                                        <td>{{ $totalCriterias }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td>Kriteria Non-Akademik</td>
+                                        <td>{{ $totalCriteriasNonAcademic }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>7</td>
+                                        <td>Sub-Kriteria Akademik</td>
+                                        <td>{{ $totalSubCriterias }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>8</td>
+                                        <td>Sub-Kriteria Non-Akademik</td>
+                                        <td>{{ $totalSubCriteriasNonAcademic }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>9</td>
+                                        <td>Kode Kriteria</td>
+                                        <td>{{ $totalCriteriaCodes }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
